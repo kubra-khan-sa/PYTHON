21cf99cae9086eb1b18e4a898e5ea389c4e43edd
A.KHATEJATHULL KUBRA <78691271+kubra-khan-sa@users.noreply.github.com>
2021-07-09 20:19:56 +0530

    Add files via upload

---
+++ brand name generator.py
@@ -0,0 +1,4 @@
+print(" Welcome to Brand Name Generator.")
+city = input("what is the city you grew up?\n")
+people = input("what are the people called as?\n")
+input("your brand name could be "+ city + "" + people)
